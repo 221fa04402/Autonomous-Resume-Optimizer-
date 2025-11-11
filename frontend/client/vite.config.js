7a8ce345de367a2ebbfd2c8ba8fb3dcf314f2a19
Ruhi <shaikruhi962@gmail.com>
2025-11-11 20:41:24 +0530

    Updated project files

--- frontend/client/vite.config.js
+++ frontend/client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",   // ✅ Required for Vercel or any static hosting
+  base: "./",  // 👈 Add this line
   server: {
     port: 5173
   }
